eed14c3b52bb00041d347e20afb29a09628eadfb
omkar0303 <81243884+omkar0303@users.noreply.github.com>
2021-06-06 13:09:21 +0530

    Update particle.js

--- particle.js
+++ particle.js
@@ -7,7 +7,7 @@ class Particle  {
         }
         this.r = r;
         this.body = Bodies.circle(x, y, this.r, options);
-        this.color=color(random(0,255),randam(0,255),randam(0,255))
+        this.color=color(random(0,255),random(0,255),random(0,255))
         World.add(world, this.body);
     }
     display() {
